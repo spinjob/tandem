490674650d499c4e4fb7af490d2a832e4e1d15e4
Spencer Johnson <spencerjohnson@macbook-pro.myfiosgateway.com>
2023-02-10 19:04:39 -0500

    Small changes

--- src/pages/api/auth/[...auth0].js
+++ src/pages/api/auth/[...auth0].js
@@ -2,7 +2,7 @@ import { handleAuth } from '@auth0/nextjs-auth0';
 
 export default handleAuth({
   onError(req, res, error) {
-    console.error(error);
+    console.log(error);
     console.log(req)
     res.status(error.status || 500).end('Check the console for the error');
   }
